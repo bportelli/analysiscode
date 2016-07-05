d8e6ef2009591eae8d24c6ab702d4be6f66ceb6d
B P <bportelli@gmail.com>
2016-07-05 17:13:34 +0100

    ...as previous

--- datasetupmaster.m
+++ datasetupmaster.m
@@ -3,39 +3,52 @@
 %% The Setup Loop
 %Set up participant data files, one participant (all datafiles) per loop
 
-while 1  
-more = input('You are about to set up (another) pp data file. Continue? y/n \n','s');
-    
-if more == 'y'
+disp('Running pp setup loop...')
 
-Readfilescript %import the Psykinematix outputs from "New Discrimination" tables
-
-[data, datacomb, mcuecomb, spdcomb, newpoints] = datafilesetup(imported, mcue, spd); %make the data and combined data tables
-
-save([pn ppcode '.mat'],'data', 'datacomb', 'mcuecomb', 'spdcomb', 'newpoints','-append') %save the new outputs
-
-ccc
-
-else
-    break
-end
-end
-
-%% The analysis loop
-% Select .mat files to analyse, then analyse them
-
-mat = 0;
+more = input('Set up a pp data file? y/n \n','s');
 while 1
-    more = input('Analyse another mat file? y/n \n','s'); %Gather up some Mat files for analysin'
-    if more == 'y'
-        mat = mat+1;
-        [fn, pn] = uigetfile('.mat');
-        Mats{mat} = [pn fn];
-        %THIS NEEDS AN UPDATE... see comment
-        copyfile('Folder Structure',pn) %NB: Give the full address of Folder structure, it will copy its contents to pn
+    if or(more == 'y', more == 'Y')
+        
+        
+%             keywds = {' MiD ',' L ','CD_','IOVD_','full_'}; %This is searched for in the expt file name
+%     mcuelist = {'MID','LAT','CD','IOVD','FULL'}; %These two variables must correspond
+%     
+%     keywdsspd = {'spd0.3','spd0.9','spd2'};
+%     spdlist = [0.3,0.9,2];
+        
+%Make Readfilescript into a function that takes in number of IV's and their lookuptables and detects them, freeing up the analysis function from needing to do any detection
+        
+        
+        Readfilescript %import the Psykinematix outputs from "New Discrimination" tables    
+        [data, datacomb, mcuecomb, spdcomb, newpoints] = datafilesetup(imported, mcue, spd); %make the data and combined data tables
+        
+        save([pn ppcode '.mat'],'data', 'datacomb', 'mcuecomb', 'spdcomb', 'newpoints','-append') %save the new outputs
+        
+        ccc
     else
         break
     end
+    more = input('Set up another pp data file? y/n \n','s');
+end
+
+%% The analysis MAT file setup loop
+% Select .mat files to analyse, then analyse them
+
+disp('Beginning anaylsis MAT file setup loop...')
+disp('Select a MAT file to analyse...')
+
+mat = 0;
+more = 'y';
+while 1
+    if or(more == 'y', more == 'Y')
+        mat = mat+1;
+        [fn, pn] = uigetfile('.mat');
+        Mats{mat} = [pn fn];
+        copyfile('C:\Users\bjp4\Documents\MATLAB\Study 3 Analysis\Folder structure',pn) %NB: Copy the Folder Structure to pn
+    else
+        break
+    end
+    more = input('Analyse another mat file? y/n \n','s');
 end
 
 if mat == 0 % Ends the script if no MAT files are selected
@@ -43,7 +56,15 @@ if mat == 0 % Ends the script if no MAT files are selected
     return
 end
 
-%% Setup
+%% Analysis loop
+disp('Beginning analysis loop...')
+
+%%Variable and function setup
+addpath(genpath('C:\Users\bjp4\Documents\MATLAB\Toolboxes')); %Make sure the Toolboxes are active
+
+%Number of IV's
+
+IVs = 2;
 
 % Table of size code - size value (chars 4:5 of the coded name)
 sizecvt = {'15',1.5;'03',3;'05',5};
@@ -51,5 +72,5 @@ sizecvt = {'15',1.5;'03',3;'05',5};
 % Table of contrast code - contrast value (chars 6:7 of the coded name)
 contcvt = {'03',3;'92',92};
 
-[] = analyse(sizecvt, contcvt, Mats); %analyse function here... this will produde the plots and excel files
+% [] = analyse(sizecvt, contcvt, Mats, IVs); %analyse function here... this will produde the plots and excel files
 
