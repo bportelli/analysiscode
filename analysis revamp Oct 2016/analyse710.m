983ddfa72d3ffaf3dc753f5cc248790a93ce46e5
B P <bportelli@gmail.com>
2016-10-07 19:24:07 +0100

    simplerupdate

--- analysis revamp Oct 2016/analyse710.m
+++ analysis revamp Oct 2016/analyse710.m
@@ -2,23 +2,23 @@
 % Just fit the function and generate the plots: keep it simple!
 % NB: This is NOT compatible with tables from demo files
 
-function outpt = analyse710()
+function [] = analyse710()
 
-sprintf('%0.0f',clock) %to give names
+%sprintf('%0.0f',clock) %to give names
 
 % Constants
-whenRun = datetime;
-runName = sprintf('%0.0f',clock);
-tempSaveDir = 'C:\Users\bjp4\Documents\MATLAB\TEMP MAT FILES\';
-
 currentTable = ?? %GET THIS
+
+whenRun = datetime;
+anaID = sprintf('%0.0f',clock);
+tempSaveDir = 'C:\Users\bjp4\Documents\MATLAB\TEMP MAT FILES\';
 
 
 % Create TEMP SAVE MAT file and LOG
-save([tempSaveDir runName '.mat'], 'whenRun');
-diary([tempSaveDir runName 'log' '.txt']);
+save([tempSaveDir anaID '.mat'], 'whenRun');
+diary([tempSaveDir anaID 'log' '.txt']);
 
-%to append use save([tempSaveDir runName '.mat'], 'VARNAME', '-append');
+%to append use save([tempSaveDir anaID '.mat'], 'VARNAME', '-append');
 
 % Ask if bootstrapping on
 [boots, ParOrNonPar]  = queryBoots(); %Currently OFF by default
@@ -33,20 +33,35 @@ thVar = getThVar(currentTable);  % Need to get the name of the current table
 PsychFunOut = getPsychFun();
 
 % Save what we've got so far
-save([tempSaveDir runName '.mat'], 'StimLevels','NumPos','OutOfNum',...
+save([tempSaveDir anaID '.mat'], 'StimLevels','NumPos','OutOfNum',...
     'ProportionCorrectObserved','StimLevelsFineGrain','PsychFunOut', '-append');
 
 
 % Run bootstrap if requested
 if boots == 1
     [bootsOut, GoFOut] = bootsfun(ParOrNonPar);
-    save([tempSaveDir runName '.mat'], 'bootsOut', 'GoFOut', '-append');
+    save([tempSaveDir anaID '.mat'], 'bootsOut', 'GoFOut', '-append');
 end
 
 
 % Make a simple plot
-makePlot()
+[fhand, tx] = makePlot();
 
+try
+T1 = outputSaveFitDetails(fhand) %This must be before closing and saving the figure
+if exist('T1','var') %append the table row to the MAT file and output it
+	%Table is output
+	writetable(T1,[tempSaveDir, anaID,'.csv']) %Writes the Table. Maybe better to make it a CSV or tab? 
+	%writetable(T1,[tempSaveDir, anaID,'.xlsx'],'Sheet','Sheet1') %Writes the Table. Maybe better to make it a CSV or tab? 
+	save([tempSaveDir anaID '.mat'], 'T1', '-append');
+end
+catch
+warning('There was an error with generating the figure table output. Threshold-AlphaEst-SlopeEst-etc. table will not be saved')
+end
+
+saveas(fhand,[tempSaveDir anaID '.fig'])
+
+%close(fhand)
 
 diary off
 
@@ -185,7 +200,7 @@ diary off
         
     end
 
-    function [] = makePlot()
+    function [fhand, tx] = makePlot()
         
         fhand = figure('name','Maximum Likelihood Psychometric Function Fitting');
         plot(StimLevels,ProportionCorrectObserved,'k.','markersize',40);
@@ -198,292 +213,53 @@ diary off
         ylabel('Proportion Correct');
         
         % Make a title
-        plottitle = [runName];
+        plottitle = [anaID];
         title(plottitle,'interpreter','none');
         
         %Threshold marker
-        tx = PF(PsychFunOut.paramsValues,0.75,'Inverse');
-        plot(tx,0.75,'bx')
-        text(tx,0.75,['  ',num2str(tx)])
+		tx = PF(paramsValues,0.75,'Inverse');
+		if and(tx<(0.9*max(StimLevels)),tx>(min(StimLevels)))
+		plot(tx,0.75,'bx')
+		text(tx,0.75,['  ',num2str(tx)])
+		else
+		plot(median(StimLevels),0.50,'bx')
+		text(median(StimLevels),0.50,['  ',num2str(tx)])
+		end
+
+		%Plot aesthetics
+		set(gca,'XTickLabelRotation', 90)
+		set(gca,'FontSize',10)
+		
     end
 
+	function T1 = outputSaveFitDetails()
+		%Storage for Excel Table
+		Threshold = tx;
+		AlphaEst = PsychFunOut.paramsValues(1); %From function fit
+		SlopeEst = PsychFunOut.paramsValues(2);
+
+		if boots == 1
+		AlphaSE = bootsOut.SD(1); %From bootstrapping for SE's
+		SlopeSE = bootsOut.SD(2);
+		Deviance = GoFOut.Dev; %From Goodness-of-fit
+		pvalue = GoFOut.pDev;
+		end
+
+		%Details appear on axes and stored in table as a row
+		if boots == 1
+		% Boots and GoF appear on axes...
+		varns = {'AlphaEst','SlopeEst','AlphaSE','SlopeSE','Deviance','pvalue'};
+		dtab = [AlphaEst; SlopeEst; AlphaSE; SlopeSE; Deviance; pvalue];
+		thand = uitable(fhand,'Data',dtab,'RowName',varns,'Position',[350 55 170 130]);
+		%... and stored in table alongside the anaID
+		T1 = table(anaID, Threshold,AlphaEst,SlopeEst,AlphaSE,SlopeSE,Deviance,pvalue);
+		else
+		varns = {'AlphaEst','SlopeEst'};
+		dtab = [AlphaEst; SlopeEst];
+		thand = uitable(fhand,'Data',dtab,'RowName',varns,'Position',[350 55 170 130]);
+		T1 = table(anaID, Threshold, AlphaEst, SlopeEst,);
+		end	
+	end
+	
+	
 end
-
-%% RESOURCES BELOW TO CANNIBALISE
-
-
-
-%% 
-
-for i = 1:length(ct)
-
-    %% Naming Setups
-    
-    tablename = ct{i};
-    
-    for v = 1:length(ivlevels) %Set up a name for output files based on IV's of this file
-        IVthisfile{v*2-1} = [setting.ivnames{v}];
-        if isnumeric(ivlevels{v}(i))
-        IVthisfile{v*2} = [num2str(ivlevels{v}(i)), '_'];     
-        else
-        IVthisfile{v*2} = [char(ivlevels{v}(i)), '_'];
-        end
-    end
-    if ~isempty(sess) %i.e. if there are session numbers, because sep. files are being analysed
-        IVthisfile(end+1) = {['S',sess.Session(i)]}; %NB: this only works because sess.Session is considered to be a list of STRINGS not numbers. If this changes, need to use num2str
-    end
-        
-%% Start calculating    
-
-
-
-diary([pn,cofref,'Outputs\',IVthisfile{:},strrep(num2str(fix(clock)),'    ',[]),'.txt'])
-disp(['Commencing Analysis of ',cofref,' data files...'])
-
-
-%% BOOTSTRAPPING FOR STANDARD ERRORS
-
-
-
-end
-%%
-%Create simple plot
-
-
- 
-
-
-diary off 
-
-%Plot aesthetics
-
-
-%Storage for Excel Table
-
-% Condition{i,1} = latormid;
-Threshold(i,1) = tx;
-% Width(i,1) = str2num(widstr);
-% Contrast(i,1) = str2num(contraststr);
-
-AlphaEst(i,1) = paramsValues(1); %From function fit
-SlopeEst(i,1) = paramsValues(2);
-
-AlphaSE(i,1) = SD(1); %From bootstrapping for SE's
-SlopeSE(i,1) = SD(2);
-
-Deviance(i,1) = Dev; %From Goodness-of-fit
-pvalue(i,1) = pDev;
-
-%Details appear on axes
-varns = {'AlphaEst','SlopeEst','AlphaSE','SlopeSE','Deviance','pvalue'};
-dtab = [AlphaEst(i,1); SlopeEst(i,1); AlphaSE(i,1); SlopeSE(i,1);Deviance(i,1); pvalue(i,1)];
-thand = uitable(fhand,'Data',dtab,'RowName',varns,'Position',[350 55 170 130]);
-
-%Save
-saveas(gcf,[pn,cofref,'Fitting\',IVthisfile{:},'_',strrep(num2str(fix(clock)),'    ',[]),'.png'])
-saveas(gcf,[pn,cofref,'Fitting\',IVthisfile{:},'_',strrep(num2str(fix(clock)),'    ',[]),'.fig'])
-
-
-close all
-
-clear StimLevels NumPos OutOfNum IVthisfile
-end
-
-T1 = table(ivlevelst{:},'VariableNames',setting.ivnames);
-T2 = table(Threshold,AlphaEst,SlopeEst,AlphaSE,SlopeSE,Deviance,pvalue);
-
-writetable([T1 sess T2],[pn,ppcode,'.xlsx'],'Sheet',xlshnm) %Writes the Excel Table. Also includes Session numbers (if they exist)
-
-% ratiotable = {[],'3 - LAT','3 - MID','92 - LAT','92 - MID';...
-%     1.50000000000000,'=N12/MIN($N$12:$Q$14)','=O12/MIN($N$12:$Q$14)','=P12/MIN($N$12:$Q$14)','=Q12/MIN($N$12:$Q$14)';...
-%     3,'=N13/MIN($N$12:$Q$14)','=O13/MIN($N$12:$Q$14)','=P13/MIN($N$12:$Q$14)','=Q13/MIN($N$12:$Q$14)';...
-%     5,'=N14/MIN($N$12:$Q$14)','=O14/MIN($N$12:$Q$14)','=P14/MIN($N$12:$Q$14)','=Q14/MIN($N$12:$Q$14)'};
-% 
-% xlswrite([pn,ppcode,'.xlsx'],ratiotable,'fromfitcom','A15');
-
-close all
-clearvars -EXCEPT cmf Mats 
-clc
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% Proper Fit Plot
-% Create a 'Plots' folder to receive!!
-
-% NB: Mats file list (cell) will be made by the Master script
-
-%function [] = analyse610(Mats)
-%indvars contains the list of independent variable levels corresponding to
-%each expt file. It's contained within Mats.
-%settings contains the settings for the expt (e.g. name and number of IV's,
-%etc)
-%combset sets whether or not the combined or individual expt files are
-%being analysed. Inputting 'comb' makes it combined.
-
-%%
-for cmf = 4%1:length(Mats)
-    
-    setting.psykvn = 'Stimulusdur';
-    
-    load(Mats{cmf}) % Load the Current Mat File
-    % Constants for this PP
-    
-    ct = fieldnames(data);
-    data2analyse = data;
-    %         ivlevels = {indvars.levelscomb}.';
-    cofref = []; %combined folder ref
-    xlshnm = 'fromfitsep'; %Excel sheet name
-    sess = table(cellfun(@(x)(x(end)),ct),'VariableNames',{'Session'});
-    
-    tic;
-for i = 1:length(ct)
-    telaps = toc;
-    waitbar(i/length(ct),sprintf('Est. time: %0.2f',telaps * (length(ct)-i)))
-    tic;
-    
-    %% Naming Setups
-    
-    tablename = ct{i};
-        
-%% Start calculating    
-
-StimLevels = data2analyse.(tablename).(setting.psykvn)'; %get the Psykinematix variable name e.g. Stimulusduration
-NumPos = data2analyse.(tablename).Response';                    
-OutOfNum = ones(1,length(NumPos));     
-
-[StimLevels NumPos OutOfNum] = PAL_PFML_GroupTrialsbyX(StimLevels, NumPos, OutOfNum);
-
-ProportionCorrectObserved=NumPos./OutOfNum; 
-StimLevelsFineGrain=[min(StimLevels):max(StimLevels)./1000:max(StimLevels)];
-
-diary([pn,cofref,'Outputs\','table',num2str(i),'_',strrep(num2str(fix(clock)),'    ',[]),'.txt'])
-disp(['Commencing Analysis of ',cofref,' data files...'])
-
-message = 'Bootstrapping on?';
-boots = 0; %input(message);
-
-message = 'Parametric Bootstrap (1) or Non-Parametric Bootstrap? (2): ';
-ParOrNonPar = 1; %input(message);
-
-if boots == 1
-    if ParOrNonPar == 1
-        disp('Parametric Bootstrap Selected');
-    else if ParOrNonPar == 2
-            disp('Non-Parametric Bootstrap Selected');
-        end
-    end
-end
-
-%Parameter grid defining parameter space through which to perform a
-%brute-force search for values to be used as initial guesses in iterative
-%parameter search.
-searchGrid.alpha = 0:.1:max(StimLevels);
-searchGrid.beta = linspace(0,100,101);
-%searchGrid.beta = logspace(1,3,100);
-searchGrid.gamma = .5;  %scalar here (since fixed) but may be vector
-searchGrid.lambda = 0.05;  %ditto
-%searchGrid.lambda = 0:.001:.1;
-
-%Threshold and Slope are free parameters, guess and lapse rate are fixed
-paramsFree = [1 1 0 0];  %1: free parameter, 0: fixed parameter
- 
-%Fit a Logistic function
-PF = @PAL_Weibull;  %Alternatives: PAL_Gumbel, PAL_Weibull, 
-                     %PAL_CumulativeNormal, PAL_HyperbolicSecant,
-                     %PAL_Logistic
-
-%Optional:
-options = PAL_minimize('options');   %type PAL_minimize('options','help') for help
-options.TolFun = 1e-09;     %increase required precision on LL
-options.MaxIter = 100;
-options.Display = 'off';    %suppress fminsearch messages
-
-%Perform fit
-disp('Fitting function.....');
-[paramsValues LL exitflag output] = PAL_PFML_Fit(StimLevels,NumPos, ...
-    OutOfNum,searchGrid,paramsFree,PF,'searchOptions',options);
-
-disp('done:')
-message = sprintf('Threshold estimate: %6.4f',paramsValues(1));
-disp(message);
-message = sprintf('Slope estimate: %6.4f\r',paramsValues(2));
-disp(message);
-
-%%
-%Create simple plot
-ProportionCorrectObserved=NumPos./OutOfNum; 
-StimLevelsFineGrain=[min(StimLevels):max(StimLevels)./1000:max(StimLevels)];
-ProportionCorrectModel = PF(paramsValues,StimLevelsFineGrain);
- 
-fhand = figure('name','Maximum Likelihood Psychometric Function Fitting');
-plot(StimLevels,ProportionCorrectObserved,'k.','markersize',10);
-set(gca, 'fontsize',16);
-set(gca, 'Xtick',StimLevels);
-axis([min(StimLevels) max(StimLevels) 0 1]);
-hold on;
-plot(StimLevelsFineGrain,ProportionCorrectModel,'g-','linewidth',4);
-
-xlabel(setting.psykvn);
-ylabel('Proportion Correct');
-
-% Make a title
-plottitle = [ppcode,num2str(i)];
-title(plottitle,'interpreter','none');
-
-%Threshold marker
-tx = PF(paramsValues,0.75,'Inverse');
-if and(tx<(0.9*max(StimLevels)),tx>(min(StimLevels)))
-    plot(tx,0.75,'bx')
-    text(tx,0.75,['  ',num2str(tx)])
-else
-    plot(median(StimLevels),0.50,'bx')
-    text(median(StimLevels),0.50,['  ',num2str(tx)])
-end
-
-diary off 
-
-%Plot aesthetics
-set(gca,'XTickLabelRotation', 90)
-set(gca,'FontSize',10)
-
-%Storage for Excel Table
-
-% Condition{i,1} = latormid;
-Threshold(i,1) = tx;
-
-%Save
-saveas(gcf,[pn,cofref,'Fitting\','table',num2str(i),'_',strrep(num2str(fix(clock)),'    ',[]),'.png'])
-saveas(gcf,[pn,cofref,'Fitting\','table',num2str(i),'_',strrep(num2str(fix(clock)),'    ',[]),'.fig'])
-
-
-close all force
-
-clear StimLevels NumPos OutOfNum IVthisfile
-
-
-
-end
-
-close all
-clearvars -EXCEPT cmf Mats 
-clc
-
-end
-
-%end
