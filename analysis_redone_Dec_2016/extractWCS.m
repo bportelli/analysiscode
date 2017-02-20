8e1710dcfd1d917e899d21a13ca55fa5e3a91e51
B P <bportelli@gmail.com>
2017-02-20 17:11:40 +0000

    IV Regex (final?)

--- analysis_redone_Dec_2016/extractWCS.m
+++ analysis_redone_Dec_2016/extractWCS.m
@@ -10,7 +10,6 @@ function [tataO] = extractWCS(tata)
 A = tata.ExpName;
 
 %% For motioncue
-
 %Lateral index
 LatIx = regexp(A,'Lateral_w');
 LatIx = ~[cellfun(@isempty,LatIx)];
@@ -30,8 +29,51 @@ MotionCues(fullIx)= {'Full'};
 MotionCues(cdIx)= {'CD'};
 MotionCues(iovdIx)= {'IOVD'};
 
-Contrasts = getNumerical('_c\d\d');
-Widths = getNumerical('_w\d(\.\d)?');
+%% Other IV's (comment out the unwanted ones)
+numpat = '\d(\.?\d)?'; %Keeping it separate in the function just in case it ever changes, but for now they're all the same (hence numpat variable)
+Contrasts = getNumerical('_c',numpat);
+Widths = getNumerical('_w',numpat);
+Speeds = getNumerical('_spd',numpat);
+
+%% Produce the output (NB: MODIFY THE insT VARIABLE IF CHANGING VARS)
+insT = table(MotionCues',Contrasts,Widths,Speeds,'VariableNames',...
+    {'MotionCues','Contrasts','Widths','Speeds'}); %table to insert
+tataO = [tata(:,1:3) insT tata(:,4:6)];
+
+
+%% Sub-functions
+
+    function cond = getNumerical(pattern1,pattern2)
+        % Takes in pattern1, the identifying word, and pattern 2, the
+        % number identifier
+        
+        %% For widths
+        bv = regexp(A,[pattern1 pattern2],'match');
+        bv(~cellfun(@isempty, bv)) = [bv{:}];
+        %bv(~cellfun(@isempty, bv)) = bv{~cellfun(@isempty, bv)};
+        %bv = [bv{:}];
+        
+        st= length(pattern1)+1;%work out where the actual number starts
+        
+        c1 = cellfun(@(x)x(st:end),bv,'Uniformoutput',0);
+        %c1 = c1';
+        
+        condi=[];
+        for k = 1:length(c1)
+            if isempty(c1{k,1})
+                continue
+            else
+                condi(k,1) = str2double(c1{k,1}); %List Complete!
+            end
+        end
+        
+        cond = condi;
+        
+    end
+
+end
+
+%% Old Code
 
 % %% For widths
 % b1 = regexp(A,'_w\d(\.\d)?','match');
@@ -69,38 +111,3 @@ Widths = getNumerical('_w\d(\.\d)?');
 
 % co = table(contrasts,'VariableNames',{'Contrasts'});
 % tata2 = [tata(:,[1,2]), co, tata(:,3:end)];
-
-
-%% Produce the output
-insT = table(MotionCues',Contrasts,Widths,'VariableNames',{'MotionCues','Contrasts','Widths'}); %table to insert
-tataO = [tata(:,1:3) insT tata(:,4:6)];
-
-
-%% Sub-functions
-
-    function cond = getNumerical(pattern)
-        % Takes in pattern
-        
-        %% For widths
-        bv = regexp(A,pattern,'match');
-        bv(~cellfun(@isempty, bv)) = [bv{:}];
-        %bv(~cellfun(@isempty, bv)) = bv{~cellfun(@isempty, bv)};
-        %bv = [bv{:}];
-        
-        c1 = cellfun(@(x)x(3:end),bv,'Uniformoutput',0);
-        %c1 = c1';
-        
-        condi=[];
-        for k = 1:length(c1)
-            if isempty(c1{k,1})
-                continue
-            else
-                condi(k,1) = str2double(c1{k,1}); %List Complete!
-            end
-        end
-        
-        cond = condi;
-        
-    end
-
-end
